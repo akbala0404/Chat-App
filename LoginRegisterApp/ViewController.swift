2ba26ea8ee6e01ef4b33fee7203173f7e87b7f1e
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2022-04-18 21:16:49 +0600

    Initial Commit

---
+++ LoginRegisterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginRegisterApp
+//
+//  Created by Акбала Тлеугалиева on 4/18/22.
+//  Copyright © 2022 Akbala Tleugaliyeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
